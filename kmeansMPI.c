f96213752d4285f86d3b26875899511e14ad49c9
Matheus <mcaldeira.matias@gmail.com>
2019-07-06 18:48:36 -0300

    kmeans MPI c parte sequencial

--- kmeansMPI.c
+++ kmeansMPI.c
@@ -8,7 +8,7 @@ int main(void) {
 	MPI_Status status;
 	int i, j, k, n, c;
 	double dmin, dx;
-	double *x, *mean, *sum;
+	double *x, *mean, *sum, *meanTemp;
 	int *cluster, *count, color;
 	int flips;
 	
@@ -49,8 +49,8 @@ int main(void) {
 			printf("enviou k=%d\n", k);
 			MPI_Send( &n, 1, MPI_INT,
 					i, tag, MPI_COMM_WORLD );
-			MPI_Send( &mean, DIM*k, MPI_DOUBLE,
-					i,tag,MPI_COMM_WORLD );
+			//MPI_Send( &mean, DIM*k, MPI_DOUBLE,
+					//i,tag,MPI_COMM_WORLD );
 			MPI_Send( &sum, DIM*k, MPI_DOUBLE,
 					i, tag, MPI_COMM_WORLD );
 			MPI_Send( &x[i*DIM*n/p], DIM*n/p, MPI_DOUBLE,
@@ -109,12 +109,12 @@ int main(void) {
 			0, tag, MPI_COMM_WORLD, &status );
 		printf("recebeu n, n=%d\n", n);
 		
-		mean = (double *)malloc(sizeof(double)*DIM*k);
+		meanTemp = (double *)malloc(sizeof(double)*DIM*k);
 		sum= (double *)malloc(sizeof(double)*DIM*k);
 		x = (double *)malloc(sizeof(double)*DIM*n);
 		
-		MPI_Recv(mean, DIM*k, MPI_DOUBLE,
-			0, tag, MPI_COMM_WORLD, &status );
+		//MPI_Recv(mean, DIM*k, MPI_DOUBLE,
+			//0, tag, MPI_COMM_WORLD, &status );
 		printf("recebeu mean\n");
 		MPI_Recv(sum, DIM*k, MPI_DOUBLE,
 			0, tag, MPI_COMM_WORLD, &status );
@@ -135,7 +135,7 @@ int main(void) {
 			}
 		printf("final do for\n");
 		//processamento do myranki
-		flips = n/p;//?sera que aqui continua n?
+		flips = n/p;
 		while (flips>0) {
 			flips = 0;
 			for (j = 0; j < k; j++) {
@@ -148,7 +148,7 @@ int main(void) {
 				for (c = 0; c < k; c++) {
 					dx = 0.0;
 					for (j = 0; j < DIM; j++) 
-						dx +=  (x[i*DIM+j] - mean[c*DIM+j])*(x[i*DIM+j] - mean[c*DIM+j]);
+						dx +=  (x[i*DIM+j] - meanTemp[c*DIM+j])*(x[i*DIM+j] - meanTemp[c*DIM+j]);
 					if (dx < dmin || dmin == -1) {
 						color = c;
 						dmin = dx;
@@ -167,7 +167,7 @@ int main(void) {
 			}
 			for (i = 0; i < k; i++) {
 				for (j = 0; j < DIM; j++) {
-					mean[i*DIM+j] = sum[i*DIM+j]/count[i];
+					meanTemp[i*DIM+j] = sum[i*DIM+j]/count[i];
 	  			}
 			}
 		}
@@ -178,25 +178,58 @@ int main(void) {
 	
 	/* Coleta do Resultado */
 	if (myrank != 0) {
-		printf("coleta myrank0\n");
-		MPI_Send(mean, 1,MPI_DOUBLE,
+		printf("coleta myrank i\n");
+		MPI_Send(meanTemp, 1,MPI_DOUBLE,
 				0,tag, MPI_COMM_WORLD);
+		printf("enviou meanTemp\n");
 	} else {
 		for (i=1; i<p; i++){
-			MPI_Recv(mean, 1, MPI_DOUBLE,
+			printf("entrou no for i=%d \n", i);
+			MPI_Recv(meanTemp, 1, MPI_DOUBLE,
 					i, tag, MPI_COMM_WORLD, &status);
+			printf("recebeu meanTemp no for i=%d \n", i);
 		}
-		
+		printf("saiu do for\n");
 	}
 
     MPI_Finalize();
-
+    
+    printf("\n");
+	printf("++++++++++++++++++++++++++++\n");
+	printf("\n");
+	printf("MPI FINALIZE\n");
+	printf("\n");
+	printf("++++++++++++++++++++++++++++\n");
+	printf("\n");
 	//parte sequencial
 	
-	//for (i = 0; i < k; i++) {
-		//dist = sqrt( pow (mean[i*DIM] - 0.0, 2) + pow (mean[i*DIM+1] - 0.0, 2) + pow (mean[i*DIM+2] - 0.0, 2) );
-	//}
-	//pontoMedio = [(x1+x2)/2, (y1+y2)/2, (z1+z2)/2]; 
+	int *flagMeanTemp;
+	flagMeanTemp = (int *)malloc(sizeof(int)*k);
+	printf("malloc flagMeanTemp\n");
+	for (i = 0; i<k; i++) {
+		flagMeanTemp[i] = 0;
+		printf("flagMeanTemp[%d] = %d", i, flagMeanTemp[i]);
+	}	
+	
+	for (int f = 0; f < k; f++) {//mean
+		double menorDist = 1000.00;
+		int indiceMeanTempMenorDist;
+		double dist;
+		for (int g = 0; g< k; g++) {//meanTemp
+			if (flagMeanTemp[g] == 0) {//so calcula dist de meanTemp[g] se esse ponto nao tiver sido utilizado ainda
+				dist = sqrt( pow (mean[f*DIM] - meanTemp[g*DIM], 2) + pow (mean[f*DIM+1] - meanTemp[g*DIM+1], 2) + pow (mean[f*DIM+2] - meanTemp[g*DIM+2], 2) );
+				if (dist<menorDist) {
+					menorDist = dist;
+					indiceMeanTempMenorDist = g;
+				}
+			}
+		}
+		//ao encontrar os dois pontos de menor distancia
+		mean[f*DIM] = (mean[f*DIM] + mean[indiceMeanTempMenorDist*DIM])/2;
+		mean[f*DIM+1] = (mean[f*DIM+1] + mean[indiceMeanTempMenorDist*DIM+1])/2;
+		mean[f*DIM+2] = (mean[f*DIM+2] + mean[indiceMeanTempMenorDist*DIM+2])/2;
+		flagMeanTemp[indiceMeanTempMenorDist] = 1;
+	}
 
 	//resultado final fica no mean
 	for (i = 0; i < k; i++) {
